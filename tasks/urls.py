fc75e693526152419d2561b3380d7b5877c85c3e
Ignaciomandalatech <ignacio@mandaltech.com.ar>
2025-01-12 21:40:51 -0300

    add url main tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,18 +1,15 @@
-from django.http import HttpResponse
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tasks import views
 
+
 router = routers.DefaultRouter()
 router.register(r'tasks',views.TaskView,'tasks')
 
-# Una vista simple para la raíz
-def root_view(request):
-    return HttpResponse("Bienvenido a la API de Tasks. Navega a /api/v1/ para usar la API.")
-
 urlpatterns = [
-    path('', root_view, name='tasks_root'),  # Maneja /tasks/
+    path('', RedirectView.as_view(url='api/v1/', permanent=True)),  # Redirige /tasks/ a /tasks/api/v1/
     path('api/v1/', include(router.urls)),  # Rutas de la API
     path('docs/', include_docs_urls(title='Task API')),  # Documentación
 ]
